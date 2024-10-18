ad2e0ff18205ed473e8950268827db0f6c99c0f2
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-10-18 18:53:58 +0300

    if file doesnt exist

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import { readFile } from "fs/promises";
 
-async function readConfig(configPath) {
+function countAlphaWords(strData) {
+  let validWords = strData.split(/\s+/);
+  validWords = validWords.filter((word) => /^[a-zA-Z]+$/.test(word));
+  return validWords.length;
+}
+
+async function getWordsCountFromFile(filePath) {
+  try {
+    const data = await readFile(filePath);
+    const count = countAlphaWords(data.toString());
+    return { filePath, count };
+  } catch (error) {
+    if (error.code == "ENOENT") {
+      return { filePath, count: 0 };
+    }
+    console.error(
+      `Got an error trying to read the file ${filePath}: ${error.message}`
+    );
+  }
+}
+
+async function getPathsFromConfigFile(configPath) {
   try {
     const data = await readFile(configPath);
     const config = JSON.parse(data);
@@ -12,24 +33,12 @@ async function readConfig(configPath) {
   }
 }
 
-async function countWords(filePath) {
+async function countAlphaWordsInFiles(configPath) {
   try {
-    const data = await readFile(filePath);
-    let validWords = data.toString().split(/\s+/);
-    validWords = validWords.filter((word) => /^[a-zA-Z]+$/.test(word));
-    const count = validWords.length;
-    return { filePath, count };
-  } catch (error) {
-    console.error(
-      `Got an error trying to read the file ${filePath}: ${error.message}`
+    const files = await getPathsFromConfigFile(configPath);
+    const promises = files.map((filePath) =>
+      getWordsCountFromFile("./" + filePath)
     );
-  }
-}
-
-async function readFiles(configPath) {
-  try {
-    const files = await readConfig(configPath);
-    const promises = files.map((filePath) => countWords("./" + filePath));
     const results = await Promise.all(promises);
 
     results.forEach(({ filePath, count }) => {
@@ -40,4 +49,4 @@ async function readFiles(configPath) {
   }
 }
 
-await readFiles("./config.json");
+await countAlphaWordsInFiles("./config.json");
